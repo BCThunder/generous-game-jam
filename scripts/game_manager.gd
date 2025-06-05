35516d43f7e10a40904444bfd39bdfde63b39b46
Brandon Cobb <thunder3000lite@gmail.com>
2025-06-04 21:31:27 -0700

    Save Small Water Data

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,5 +1,9 @@
 extends Node
 
 var can_player_interact := false
+var tile_data := []
+var collected_water := 0
 
-	
+func remember_water(tilemap: TileMapLayer, file_path: String):
+	for tile in tilemap.get_children(): # Layer 0, change if needed
+		tile_data.append(tile.monitorable)
--- scripts/save_manager.gd
+++ scripts/save_manager.gd
@@ -2,23 +2,32 @@ extends Node
 
 var config = ConfigFile.new()
 var player
+var save_path := "res://game_save.cfg"
+
 
 func _ready():
 	player = get_parent().get_node("Player")
 
+
 func save_game():
 	print("Checkpoint Location to save: ", $%CheckpointManager.last_position)
 	config.set_value("Player", "position", $%CheckpointManager.last_position)
-	config.save("res://game_save.cfg")
-	print("Game Saved!")
+	config.set_value("Level", "small_water", GameManager.tile_data)
+	config.save(save_path)
+	
+	var error := config.save(save_path)
+	if error:
+		print("An error happened while saving data: ", error)
+
 
 func load_save():
-	var save_file = config.load("res://game_save.cfg")
+	var save_file = config.load(save_path)
 	if save_file == OK:
 		var player_last_location = config.get_value("Player", "position")
+		GameManager.tile_data = config.get_value("Level", "small_water")
 		print("Spawn character at ", player_last_location)
 		$%CheckpointManager.update_checkpoint(player_last_location)
-		print("Save Loaded!")
+
 
 
 func _on_save_button_pressed() -> void:
